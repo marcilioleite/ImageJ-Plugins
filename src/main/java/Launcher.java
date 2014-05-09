6e7a404b2b6002f6fa4a3122be96557c123ea01e
Marcilio Leite <marcilioleite@live.com>
2014-05-09 16:51:08 -0300

    Primeiro commit

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,19 @@
+
+import ij.ImageJ;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marcilio
+ */
+public class Launcher {
+    
+    public static void main(String args[]) {
+        ImageJ imagej = new ImageJ();
+    }
+}
---
+++ src/main/java/My_Plugin.java
@@ -0,0 +1,22 @@
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marcilio
+ */
+public class My_Plugin implements PlugIn {
+
+    @Override
+    public void run(String string) {
+        IJ.showMessage("Blablabla");
+    }
+    
+}
